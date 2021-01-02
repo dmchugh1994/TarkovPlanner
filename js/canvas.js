279fb11a5dbf609e6842904660c76fed2f99e80b
Daniel McHugh <dmchugh1994@gmail.com>
2021-01-02 20:25:54 +0800

    refactor and update to map objects

--- js/canvas.js
+++ js/canvas.js
@@ -1,94 +1,72 @@
-var context, mapOverlay;
+var mapOverlay;
 var map;
+var maps = {};
+var selectedMap;
 
-const interchangeURL = 'images/interchange.png';
+maps.interchange = new MapObject('images/interchange.png', 6626, 3584);
+maps.customs = new MapObject('images/customs.png', 3840, 1920);
+maps.customsDorms = new MapObject('images/customsDorms.png', 1572, 859);
 
 // Check for the canvas tag onload. 
 if (window.addEventListener) {
     window.addEventListener('load', function () {
 
-        function init() {
-
-            var map_select = document.getElementById('selector');
-            if (!map_select) {
-                alert('Error! Failed to get the select element!');
-                return;
-            }
-            map_select.addEventListener('change', ev_map_change, false);
-
-            window.addEventListener("contextmenu", (e) => { e.preventDefault(); return false; });
-
-            map = L.map('map', {
-                minZoom: 0.1,
-                maxZoom: 3,
-                center: [0, 0],
-                zoom: 2,
-                crs: L.CRS.Simple
-            });
-
-            var bounds = getImageBounds(interchangeURL);
-
-            // add the image overlay, 
-            // so that it covers the entire map
-            mapOverlay = L.imageOverlay(interchangeURL, bounds).addTo(map);
-
-            // tell leaflet that the map is exactly as big as the image
-            map.setMaxBounds(bounds);
-
-            window.onresize = function () {
-                resize();
-            };
+        var mapSelector = document.getElementById('selector');
+        if (!mapSelector) {
+            alert('Error! Failed to get the select element!');
+            return;
         }
+        mapSelector.addEventListener('change', changeMap, false);
+
+        window.addEventListener("contextmenu", (e) => { e.preventDefault(); return false; });
+
+        map = L.map('map', {
+            minZoom: 0.1,
+            maxZoom: 3,
+            center: [0, 0],
+            zoom: 2,
+            crs: L.CRS.Simple
+        });
+
+        resize();
+
+        var bounds = getImageBounds(maps.interchange);
+
+        // add the image overlay, 
+        // so that it covers the entire map
+        mapOverlay = L.imageOverlay(maps.interchange.url, bounds).addTo(map);
+
+        // tell leaflet that the map is exactly as big as the image
+        map.setMaxBounds(bounds);
+
+        window.onresize = function () {
+            resize();
+        };
 
         function resize() {
             var heights = window.innerHeight;
             document.getElementById("map").style.height = heights - 40 + "px";
         }
-        resize();
 
-        function getImageBounds(imageURL) {
-            var w, h;
-
-            var img = new Image();
-            img.src = imageURL;
-
-            w = img.width;
-            h = img.height;
-
+        function getImageBounds(mapToLoad) {
             // calculate the edges of the image, in coordinate space
-            var southWest = map.unproject([0, h], map.x() - 1);
-            var northEast = map.unproject([w, 0], map.getMaxZoom() - 1);
-            var bounds = new L.LatLngBounds(southWest, northEast);
+            var southWest = map.unproject([0, mapToLoad.height], map.getMaxZoom() - 1);
+            var northEast = map.unproject([mapToLoad.width, 0], map.getMaxZoom() - 1);
 
-            return bounds;
+            return new L.LatLngBounds(southWest, northEast);
         }
 
-        function ev_map_change(ev) {
+        function changeMap(ev) {
             if (maps[this.value]) {
-                map = new maps[this.value]();
+                selectedMap = maps[this.value];
 
-                console.log("changing map");
+                mapOverlay.setMaxBounds = getImageBounds(selectedMap);
+                mapOverlay.setUrl(selectedMap.url);
+
+                console.log("Changing map: " + this.value);
             }
         }
 
-        var maps = {};
-
-        // Maps. 
-        maps.interchange = function () {
-            var imageURL = interchangeURL;
-            mapOverlay.setMaxBounds = getImageBounds(interchangeURL);
-            mapOverlay.setUrl(imageURL);
-        };
-
-        maps.customs = function () {
-            mapOverlay.setUrl('images/customs.png');
-        };
-
-        maps.customsDorms = function () {
-            mapOverlay.setUrl('images/customsDorms.png');
-        };
-
-        init();
-
     }, false);
 }
+
---
+++ js/mapObject.js
@@ -0,0 +1,8 @@
+class MapObject {
+    constructor(url, width = 0, height = 0) {
+        this.url = url;
+        this.height = height;
+        this.width = width;
+    }
+
+}
